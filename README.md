3db41ab2c0cc310a21c515553c704e76c8d8b7bb
nickcamp13 <nicholascampos013@gmail.com>
2024-08-08 02:32:58 -0700

    cpu and player moves

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rock Paper Scissors
+
+## Plan
+
+1) Get the CPU's choice
+2) Get the Player's choice
+3) Write logic for a single round
+   1) generate cpu choice
+   2) ask for users choice
+   3) display choices
+   4) display winner
+   5) display scores
+   6) ask to play another round
+4) Write logic for an entire game
+   1) 
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const moves = ["rock", "paper", "scissors"];
+let p1Score = 0;
+let p2Score = 0;
+
+function getRandMove() {
+    index = Math.floor(Math.random() * 3);
+    return moves[index];
+}
+
+function getCPUChoice() {
+    return getRandMove();
+}
+
+function getPlayerChoice() {
+    let playerChoice = undefined    
+    while (!moves.includes(playerChoice)) {
+        playerChoice = window.prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase()
+        console.log(playerChoice);
+    }
+}
